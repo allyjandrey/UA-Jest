fc02af0062cc45be352e9ae406bbe95f813c4831
Allyson <jandreyallyson@gmail.com>
2023-04-12 20:39:25 -0300

    Atividade

--- src/Pessoa.js
+++ src/Pessoa.js
@@ -7,7 +7,7 @@
  * - Telefone
  * - Tempo de trabalho
  * 
- * Essa classe deve ter um construtr publico para instânciar os atributos acima
+ * Essa classe deve ter um construtor publico para instânciar os atributos acima
  * 
  * Os métodos são:
  * -> getNome > Retorna o nome
@@ -16,11 +16,12 @@
  * -> getDadosCompletos > Retorna concatenando NOME, IDADE, GÊNERO e TELEFONE
  * -> getNomeETelefone > Retorna o nome e telefone
  * -> getTempoDeTrabalho > Retorna o tempo de trabalho
- * -> getTempoParaSeAposentar > Assuma que a idade de aposentadoria e 60 anos
- * -> getTelefone > Retorna o telefone
+ * -> getTempoParaSeAposentar > Assuma que a idade de aposentadoria e 60 anos feminino, 65 anos masculino
+ * -> getTelefone > Retorna o telefone.
  * 
  * Em seguida crie uma classe de teste para PESSOA e cubra as funcionalidades 
  * com testes unitários para validar o correto funcionamento da mesma.
+ * 
  */
 
 class Pessoa {
