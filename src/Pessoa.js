de184acf8bf7baeda8bc34e5196b7a90a075823a
Allyson <jandreyallyson@gmail.com>
2023-04-12 20:29:48 -0300

    Atividade

---
+++ src/Pessoa.js
@@ -0,0 +1,28 @@
+/**
+ * Criar uma classe PESSOA e adicionar os seguintes atributos para ela:
+ * - Nome
+ * - Idade
+ * - Endereço
+ * - Gênero
+ * - Telefone
+ * - Tempo de trabalho
+ * 
+ * Essa classe deve ter um construtr publico para instânciar os atributos acima
+ * 
+ * Os métodos são:
+ * -> getNome > Retorna o nome
+ * -> getIdade > Retorna a idade
+ * -> getEndereço > Retorna o endereço
+ * -> getDadosCompletos > Retorna concatenando NOME, IDADE, GÊNERO e TELEFONE
+ * -> getNomeETelefone > Retorna o nome e telefone
+ * -> getTempoDeTrabalho > Retorna o tempo de trabalho
+ * -> getTempoParaSeAposentar > Assuma que a idade de aposentadoria e 60 anos
+ * -> getTelefone > Retorna o telefone
+ * 
+ * Em seguida crie uma classe de teste para PESSOA e cubra as funcionalidades 
+ * com testes unitários para validar o correto funcionamento da mesma.
+ */
+
+class Pessoa {
+
+}
